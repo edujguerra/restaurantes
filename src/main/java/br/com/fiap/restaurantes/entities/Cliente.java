4a8bea0e107b45413fa0366b52c43645d4bd0840
Thiago Vargas <thiagorv16957@gmail.com>
2024-03-20 17:32:59 -0300

    correcao entidade

--- src/main/java/br/com/fiap/restaurantes/entities/Cliente.java
+++ src/main/java/br/com/fiap/restaurantes/entities/Cliente.java
@@ -1,12 +1,20 @@
 package br.com.fiap.restaurantes.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "tb_cliente")
 public class Cliente extends Pessoa {
 
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "id_cliente", nullable = false)
+   private Long id;
+
    public Cliente(Long id, String nome, String email, Long telefone) {
-      super(id,nome,email,telefone);
+      super(nome,email,telefone);
+      this.id = id;
    }
 }
