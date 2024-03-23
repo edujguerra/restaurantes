26d5a3e6763ebe2477df404f46b11c57d1aeb145
Julio <juliobcp@gmail.com>
2024-03-23 20:48:17 -0300

    Alteracoes para resolucao de conflito e testes

--- src/main/java/br/com/fiap/restaurantes/service/ReservaService.java
+++ src/main/java/br/com/fiap/restaurantes/service/ReservaService.java
@@ -9,7 +9,7 @@ public interface ReservaService {
 
     public Collection<ReservaDTO> findAll();
 
-    public ReservaDTO findById(Long id);
+    public Reserva findById(Long id);
 
     public ReservaDTO save(ReservaDTO reservaDTO);
 
